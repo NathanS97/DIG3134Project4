f5d1e99a070919b2731b102fb8d41c2b606ddcd7
NathanS97 <42750605+nSnod@users.noreply.github.com>
2019-11-21 18:47:10 -0500

    new commit - NS

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php
+  // function for users to delete their comment, call in button/hyperlink - NS
+  function delCom($conn) {
+    if (isset($_POST['commentDelete'])) {
+      $commentID = $_POST['cid'];
+
+      $sql = "DELETE FROM comments WHERE cid='$cid'";
+      $result = $conn->query($sql);
+      header("location: index.php");
+    }
+  }
+?>
