22a0590665906d6979f686b62715a4fc1af10b61
NathanS97 <42750605+nSnod@users.noreply.github.com>
2019-11-23 11:35:11 -0500

    new commit - NS

---
+++ reviews.php
@@ -0,0 +1,27 @@
+<?php
+  include './common/start.php';
+  start();
+  logout();
+
+ ?>
+<?php include './templates/header.php'; ?>
+    <link rel="stylesheet" href="public/style/main.css">
+    <link rel="stylesheet" href="public/style/mediaquery.css">
+<?php include './templates/nav.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Reviews</title>
+</head>
+<body>
+  <div class="wrapper">
+    <form action="" method="post">
+      <input type="button" value="">
+    </form>
+  </div>
+</body>
+</html>
+<?php include './templates/footer.php' ?>
